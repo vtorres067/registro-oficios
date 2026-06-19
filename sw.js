6091eecce281198b789a288d23bd9dacafca2a2a
vtorres067 <vtorres067@users.noreply.github.com>
2026-06-19 09:51:39 -0300

    Fix: add immediate UI update after save, fix SW caching

--- sw.js
+++ sw.js
@@ -1,25 +1,18 @@
-const CACHE = 'registro-oficios-v1';
-const URLS = [
-  '/registro-oficios/',
-  '/registro-oficios/index.html',
-  '/registro-oficios/manifest.json',
-  '/registro-oficios/icon-192.png',
-  '/registro-oficios/icon-512.png'
-];
+const CACHE = 'registro-oficios-v2';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(URLS))
-  );
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', () => {
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+    ))
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
